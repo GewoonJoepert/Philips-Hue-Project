d225127bd554eb28873e537d1de639363a67592e
JameyErkelens <Jamey.Erkelens@coderclass.nl>
2018-06-21 10:00:42 +0200

    ding weg gehaald

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ oranje.addEventListener("click", veranderOranje)
 
 function veranderRood(){
 	for (var i = 0; i < lampen.length; i++) {
-		sendRequest(lampen[i],'{"hue": 0, "bri": 80	}');
+		sendRequest(lampen[i],'{"hue": 0, "bri": 120	}');
 	}
 }
 
 function veranderGroen(){
 	for (var i = 0; i < lampen.length; i++) {
-		sendRequest(lampen[i],'{"hue": 30000, "bri": 80	}');
+		sendRequest(lampen[i],'{"hue": 30000, "bri": 120	}');
 	}
 }
 function veranderOranje(){
 	for (var i = 0; i < lampen.length; i++) {
-		sendRequest(lampen[i],'{"hue": 7000, "bri": 80	}');
+		sendRequest(lampen[i],'{"hue": 7000, "bri": 120	}');
 	}
 }
 function sendRequest(lamp, body){
